362f568ae1f2193cbc1399276890c3e7445a1a13
oghusky <philiposimmons@gmail.com>
2021-02-01 19:31:06 -0500

    Subject: get and post ratings
    Body:Able to post and get ratings

---
+++ models/Rating.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const RatingSchema = new mongoose.Schema({
+  fname: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lname: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+  },
+  rating: {
+    type: Number
+  },
+  comment: {
+    type: String,
+    trim: true,
+    required: true,
+    minlength: 10,
+    maxlength: 200
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Rating", RatingSchema);
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express'),
   app = express(),
+  Rating = require('./models/Rating'),
   connectDB = require('./config/connectDB'),
   PORT = process.env.PORT || 3001;
 
@@ -11,6 +12,23 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/config", (req, res) => {
   res.json({ msg: "server running" })
 })
+
+app.post("/api/rate_us", async (req, res) => {
+  const { fname, lname, email, rating, comment } = req.body;
+  if (fname && lname && rating && comment) {
+    const newRate = new Rating({ fname, lname, email, rating, comment });
+    await newRate.save();
+    return res.status(201).json({ msg: "Rating created", newRate })
+  }
+});
+
+app.get("/api/ratings", async (req, res) => {
+  const rates = await Rating.find();
+  const ratings = rates.map(rate => {
+    return { fname: rate.fname, lname: rate.lname[0], comment: rate.comment }
+  });
+  return res.status(200).json({ msg: "Found Ratings", ratings })
+});
 
 app.listen(PORT, () => {
   console.log("http://localhost:3001")
